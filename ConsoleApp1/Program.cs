b297a22e39a37464d061b3e73129d8b2f4d5fb54
js050225 <js050225@gmail.com>
2019-09-13 15:19:34 +0900

    study

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,31 +10,58 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("안녕하세요"); //출력문
-            string a = Console.ReadLine(); //스트림 입력문
-            Console.WriteLine("사용자가" + a + " 라고 입력하였습니다"); //스트림 출력문
+            //Console.WriteLine("안녕하세요"); //출력문
+            //string a = Console.ReadLine(); //스트림 입력문
+            //Console.WriteLine("사용자가" + a + " 라고 입력하였습니다"); //스트림 출력문
 
 
 
-            if(a == "안녕")
+            //if (a == "안녕")
+            //{
+            //    Console.WriteLine("다시한번 안녕");
+            //}
+            //else if (a == "아니 안녕못해")
+            //{
+            //    Console.WriteLine("ㅠㅠ");
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine("뭐라고 말좀해봐");
+            //}
+
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    Console.WriteLine(i + 1 + "회차 진행중...");
+            //}
+
+            //Console.ReadLine();
+
+            /////////////////////////////////////////////////////////--응용 Paise -------------
+            Console.WriteLine("계산기");
+            Console.Write("첫번째숫자-->");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.Write("두번째숫자는-->");
+            int y = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            
+            Console.WriteLine("부호는(-나+ 만가능) ");
+            string z = Console.ReadLine();
+            if (z == "+")
             {
-                Console.WriteLine("다시한번 안녕");
+                Console.WriteLine("더한 결과는 :" + (x + y) + "입니다");
             }
-            else if(a == "아니 안녕못해")
+            else if (z == "-")
             {
-                Console.WriteLine("ㅠㅠ");
-                
+                Console.WriteLine("뺀 결과는:" + (x - y) + "입니다");
             }
             else
             {
-                Console.WriteLine("뭐라고 말좀해봐");
-            }
-
-            for(int i = 0; i < 5; i++)
-            {
-                Console.WriteLine(i + 1 + "회차 진행중...");
+                Console.WriteLine("버젼이 낮아서 안되");
             }
             Console.ReadLine();
+
 
 
         }
